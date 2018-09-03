b3d9fdcf600749bf211a24b4f1ab20134ce2208d
Jchen <jcdigital316@gmail.com>
2018-09-02 23:12:56 -0700

    added through2-map npm and finished HTTP UpperCase

--- program.js
+++ program.js
@@ -148,11 +148,27 @@
 
 //HTTP FILE Server
 
-const http = require('http')
-const fs = require('fs')
+// const http = require('http')
+// const fs = require('fs')
 
-const server = http.createServer( function (req, res)  {
-  fs.createReadStream(process.argv[3]).pipe(res)
+// const server = http.createServer( function (req, res)  {
+//   fs.createReadStream(process.argv[3]).pipe(res)
+// })
+
+// server.listen(process.argv[2])
+
+//HTTP UpperCaser
+
+const http = require("http")
+const fs = require('fs')
+const map = require('through2-map')
+
+const server = http.createServer( function (req, res) {
+  if(req.method === "POST"){
+    req.pipe(map(function (chunk){
+      return chunk.toString().toUpperCase()
+    })).pipe(res)
+  }
 })
 
 server.listen(process.argv[2])
